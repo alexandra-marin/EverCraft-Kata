076c219038be1aeccec6ebb04458fa6c436c1d7a
Alexandra Marin <alexandra.marin01@gmail.com>
2017-08-30 01:35:18 +0300

    Added webpack + babel loader

--- javascript/karma.conf.js
+++ javascript/karma.conf.js
@@ -28,8 +28,21 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+        "spec/*.spec.js": ["webpack"]
     },
 
+    webpack: {
+        module: {
+            loaders: [
+                {
+                    test: /\.js/,
+                    exclude: /node_modules/,
+                    loader: "babel-loader"
+                }
+            ]
+        },
+        watch: true
+    },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
