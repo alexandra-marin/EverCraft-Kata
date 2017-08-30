ec9d8b39537e76952fefbefdee5a4d1dca130242
Alexandra Marin <alexandra.marin01@gmail.com>
2017-08-30 03:56:45 +0300

    Attack and damage

--- javascript/spec/Character.spec.js
+++ javascript/spec/Character.spec.js
@@ -2,33 +2,99 @@ import { should } from "should";
 import { Character, Alignments } from "./src/Character";
 
 describe("Character should", function() {
-    let character;
+	let character;
 
-    beforeEach(() => {
+	beforeEach(() => {
 		character = new Character();
-    })
+	});
 
 	it("have a name", function() {
 		character.should.have.property("name");
-    });
+	});
 
-    it("have an alignment", function() {
+	it("have an alignment", function() {
 		character.should.have.property("alignment");
-    });
+	});
 
-    it("not have alignment different than Good, Bad, Neutral", function() {
-        character.alignment = Alignments.good;
-        character.alignment = 'Unknown';
+	it("not have alignment different than Good, Bad, Neutral", function() {
+		character.alignment = Alignments.good;
+		character.alignment = "Unknown";
 		character.alignment.should.equal(Alignments.good);
-    });
+	});
 
-    it("have 5 HP", function() {
+	it("have 5 HP", function() {
 		character.should.have.property("hitPoints");
 		character.hitPoints.should.be.exactly(5);
-    });
+	});
 
-    it("have 10 armor", function() {
+	it("have 10 armor", function() {
 		character.should.have.property("armor");
 		character.armor.should.be.exactly(10);
+	});
+
+	it("attack by rolling a dice 20-sided dice", function() {
+		var attack = character.rollDice();
+		console.log("Dice roll: " + character.attackForce);
+		character.attackForce.should.be.aboveOrEqual(1).and.belowOrEqual(20);
+	});
+
+	it("inflict damage if roll is equal or greater than enemy armor", function() {
+        const enemy = new Character();
+        enemy.armor = 10;
+
+        character.attackForce = 15;
+        const willDamage = character.canDamage(enemy.armor);
+
+        willDamage.should.be.true();
+	});
+
+	it("not inflict damage if roll is less than enemy armor", function() {
+		const enemy = new Character();
+        enemy.armor = 10;
+
+        character.attackForce = 5;
+        const willDamage = character.canDamage(enemy.armor);
+
+        willDamage.should.be.false();
     });
+    
+    it("take 1 damage when attack is greater or equal to armor", function() {
+		const enemy = new Character();
+        enemy.attackForce = 15;
+
+        character.armor = 2;
+        character.hitPoints = 5;
+        character.takeDamage(enemy.attackForce);
+
+        character.hitPoints.should.be.exactly(4);
+    });
+    
+    it("take no damage when attack is less than armor", function() {
+		const enemy = new Character();
+        enemy.attackForce = 15;
+
+        character.armor = 20;
+        character.hitPoints = 5;
+        character.takeDamage(enemy.attackForce);
+
+        character.hitPoints.should.be.exactly(5);
+    });
+    
+    it("take double damage when attack 20", function() {
+		const enemy = new Character();
+        enemy.attackForce = 20;
+
+        character.armor = 20;
+        character.hitPoints = 5;
+        character.takeDamage(enemy.attackForce);
+
+        character.hitPoints.should.be.exactly(3);
+    });
+    
+    it("die if hit points is 0 or less", function() {
+        character.hitPoints = 0;
+        var willDie = character.willDie();
+
+        willDie.should.be.true();
+	});
 });
--- javascript/spec/src/Character.js
+++ javascript/spec/src/Character.js
@@ -4,18 +4,51 @@ export class Character {
 		this.hasAlignment = Alignments.good;
 		this.hitPoints = 5;
 		this.armor = 10;
+		this.attackForce = 1;
 	}
 
 	get alignment() {
 		return this.hasAlignment;
-    }
-    
+	}
+
 	set alignment(value) {
-        var alignmentExists = Alignments[value];
-        
-        if (alignmentExists) {
+		var alignmentExists = Alignments[value];
+
+		if (alignmentExists) {
 			this.hasAlignment = value;
 		}
+	}
+
+	rollDice() {
+		this.attackForce = getRandomInt(1, DiceSides);
+		return this.attackForce;
+	}
+
+	canDamage(armor) {
+		if (armor < this.attackForce) return true;
+		return false;
+	}
+
+	takeDamage(attack) {
+		if (attack === 20) {
+			return this.doubleDamage();
+		}
+
+		if (this.armor <= attack) {
+			return this.normalDamage();
+		}
+	}
+
+	normalDamage() {
+		this.hitPoints -= NormalDamage;
+	}
+
+	doubleDamage() {
+		this.hitPoints -= 2 * NormalDamage;
+	}
+
+	willDie() {
+		return this.hitPoints <= 0;
 	}
 }
 
@@ -24,3 +57,10 @@ export const Alignments = {
 	bad: "Bad",
 	neutral: "Neutral"
 };
+
+const DiceSides = 20;
+const NormalDamage = 1;
+
+function getRandomInt(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
